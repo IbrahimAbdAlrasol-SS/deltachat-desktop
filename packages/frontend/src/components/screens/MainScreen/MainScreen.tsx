568a000320affbdbd0338bfe882b9d783a4e86fe
WofWca <wofwca@protonmail.com>
2025-05-02 18:09:35 +0400

    fix: a11y: unmark "Chats" section header as <nav>
    The "Chats" and the "Messages" sections headers used to be
    one single element, then it has been split into two in
    66c96d7df19f591a8c8610842952cb9f7b9ed540
    (https://github.com/deltachat/deltachat-desktop/pull/4672).
    But then it started to make even less sense for it to be a <nav>.
    
    It basically consists of only two elements,
    so it's probably not worth having a separate landmark for it.
    Let's turn it into a generic <section> with no label.

--- packages/frontend/src/components/screens/MainScreen/MainScreen.tsx
+++ packages/frontend/src/components/screens/MainScreen/MainScreen.tsx
@@ -185,8 +185,8 @@ export default function MainScreen({ accountId }: Props) {
         !messageSectionShouldBeHidden ? 'chat-view-open' : ''
       }`}
     >
-      <section className={styles.chatListAndNavbar}>
-        <nav className={styles.chatListNavbar} data-tauri-drag-region>
+      <section className={styles.chatListAndHeader}>
+        <section className={styles.chatListHeader} data-tauri-drag-region>
           {showArchivedChats && (
             <>
               <span data-no-drag-region>
@@ -212,7 +212,7 @@ export default function MainScreen({ accountId }: Props) {
               value={queryStr}
             />
           )}
-        </nav>
+        </section>
         <ChatList
           queryStr={queryStr}
           showArchivedChats={showArchivedChats}
